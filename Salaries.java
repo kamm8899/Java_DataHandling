98cc3fc5808208d25f3992625d8638509a0c3104
kamm8899 <kamm8899@gmail.com>
2021-11-30 17:57:30 -0800

    added Question 1

--- Salaries.java
+++ Salaries.java
@@ -3,6 +3,26 @@ public class Salaries {
    //Write a program to accept 5 employee IDs and corresponding names and their salareies
    //from user and store them in three arrays. Pass these arrays to a function display() as arguments
    // This display() will display the content of the arrays in the following format. 
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        //establish 3 arrays
+        int id[]= new int[5];
+        int salary[]= new int[5];
+        int i;
+        String names[]= new String[5];
 
+        for(i=0; i<5; i++){
+            System.out.println("Enter the employee id");
+            id[i]= input.nextInt();
+            System.out.println("Enter the employees salary");
+            salary[i]=input.nextInt();
+            System.out.println("Enter the employee name");
+            names[i]= input.nextInt();
+        }
+        System.out.println("ID Name Salary");
+        for(i=0; i<5; i++){
+            System.out.println("String.format('%4d%8s%14d'", id[i], name[i], salary[i]);
+        }
+    }
 
 }
