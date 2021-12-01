a68c7047024660b677960f4e8a85019baf378715
kamm8899 <kamm8899@gmail.com>
2021-11-30 17:47:15 -0800

    first commit

---
+++ Salaries.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Salaries {
+   //Write a program to accept 5 employee IDs and corresponding names and their salareies
+   //from user and store them in three arrays. Pass these arrays to a function display() as arguments
+   // This display() will display the content of the arrays in the following format. 
+
+
+}
